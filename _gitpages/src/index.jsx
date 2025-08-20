23cc3a508c145b119963016abe514b41a4cbecba
Enjl <enjlectric@gmail.com>
2025-08-20 22:46:24 +0900

    this might work?

--- _gitpages/src/index.jsx
+++ _gitpages/src/index.jsx
@@ -1,8 +1,7 @@
 import { render } from 'preact';
-import { LocationProvider, Router, Route, useLocation } from 'preact-iso';
+import { Router, Route } from 'preact-router';
 
 import img_logo from "./assets/logoo.png"
-import { Header } from './components/Header.jsx';
 import { Nyxel } from './components/Nyxel.jsx';
 import { Home } from './pages/Home/index.jsx';
 import Blog from './pages/blog/index.jsx';
@@ -13,25 +12,19 @@ import { useContext } from 'preact/hooks';
 import { createHashHistory } from "history";
 
 export function App() {
-	const {path} = useLocation();
 	return (
-		<LocationProvider>
-			{/* <Header /> */}
 			<main className="gluten-light">
 				<br></br>
 				<img className="center" src = {img_logo}></img>
 				<TransitionGroup component = {null}>
-					<CSSTransition key={path} classNames="fade" timeout={300}>
-						<Router history={(createHashHistory())}>
-							<Route path="/" component={Home} />
-							<Route path="/blog" component={Blog} />
-							<Route default component={NotFound} />
-						</Router>
-					</CSSTransition>
+					<Router history={(createHashHistory())}>
+						<Route path="" component={Home} />
+						<Route path="blog" component={Blog} />
+						<Route default component={NotFound} />
+					</Router>
 				</TransitionGroup>
 				{/* <Nyxel></Nyxel> */}
 			</main>
-		</LocationProvider>
 	);
 }
 
